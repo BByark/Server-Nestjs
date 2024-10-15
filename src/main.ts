36b7d95a19491c4dc4f1b118b41a559304a8c0a4
siniseong <shinhuiseong07@gmail.com>
2024-10-16 02:32:10 +0900

    ✨ Swagger UI 사용

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+  .setTitle('API Title')
+  .setDescription('Your API description')
+  .setVersion('1.0')
+  .addServer('http://localhost:3000/', 'Local environment')
+  .addServer('https://staging.yourapi.com/', 'Staging')
+  .addServer('https://production.yourapi.com/', 'Production')
+  .addTag('Your API Tag')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/', app, document);
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
