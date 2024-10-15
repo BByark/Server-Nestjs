4e5268ce9c86ebd75b87709bb35b3392eaf7d122
siniseong <shinhuiseong07@gmail.com>
2024-10-16 01:51:15 +0900

    ✨ Mongodb atlas 접속

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import * as mongoose from 'mongoose';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGODB_URL)],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  private readonly isDev: boolean = process.env.NODE_ENV === 'dev' ? true : false;
+
+  configure() {
+    console.log('MONGODB_URL:', process.env.MONGODB_URL); 
+    mongoose.set('debug', this.isDev);
+  }
+}
