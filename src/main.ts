279d94d64515926d7d18b4de872840fe606192ac
siniseong <shinhuiseong07@gmail.com>
2024-10-16 02:45:14 +0900

    ✏️ Swgger Tag 및 세부 내용 수정

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,14 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const options = new DocumentBuilder()
-  .setTitle('API Title')
-  .setDescription('Your API description')
-  .setVersion('1.0')
-  .addServer('http://localhost:3000/', 'Local environment')
-  .addServer('https://staging.yourapi.com/', 'Staging')
-  .addServer('https://production.yourapi.com/', 'Production')
-  .addTag('Your API Tag')
-  .build();
+    .setTitle('BByark API')
+    .setDescription('🐤 삐약이 API들')
+    .setVersion('v0.9')
+    .addServer('http://localhost:3000/', 'Local environment')
+    .addTag('Admin')
+    .addTag('teacher')
+    .addTag('student')
+    .build();
 
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('/', app, document);
