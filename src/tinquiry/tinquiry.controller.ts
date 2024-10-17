eea9b772aa7d73cd6612caccc22bf04c297fa3f7
siniseong <shinhuiseong07@gmail.com>
2024-10-17 14:44:48 +0900

    ✨ Tinquiry apitag 수정

--- src/tinquiry/tinquiry.controller.ts
+++ src/tinquiry/tinquiry.controller.ts
@@ -3,13 +3,13 @@ import { TinquiryService } from './tinquiry.service';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { Teacher } from './schemas/teacher.schema'; 
 
-@ApiTags('교사 조회')
-@Controller('api/teacher')
+@ApiTags('교사')
+@Controller('teacher')
 export class TinquiryController {
   constructor(private readonly tinquiryService: TinquiryService) {}
 
   @Get(':id')
-  @ApiOperation({ summary: '특정 교사 정보 조회' })
+  @ApiOperation({ summary: '교사 정보 조회' })
   @ApiResponse({ status: 200, description: '교사 정보', type: Teacher })
   async getTeacher(@Param('id') id: string): Promise<Teacher> {
     return this.tinquiryService.getTeacherById(id);
