2cd9680e6149843ffd182c2fe90dd89274bfada1
siniseong <shinhuiseong07@gmail.com>
2024-10-18 09:37:40 +0900

    🐛 환경 변수 설정 가

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,9 @@ import { GetinsertModule } from './getinsert/getinsert.module';
 import { NotesModule } from './notes/notes.module';
 import { TinquiryModule } from './tinquiry/tinquiry.module';
 import * as mongoose from 'mongoose';
+import * as dotenv from 'dotenv';  
+
+dotenv.config(); 
 
 @Module({
   imports: [
@@ -29,7 +32,7 @@ export class AppModule implements NestModule {
   private readonly isDev: boolean = process.env.NODE_ENV === 'dev';
 
   configure() {
-    console.log('MONGODB_URI:', process.env.MONGODB_URI); 
+    console.log('MONGODB_URI:', process.env.MONGODB_URI);
     mongoose.set('debug', this.isDev);
   }
 }
