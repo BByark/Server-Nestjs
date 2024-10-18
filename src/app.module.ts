91c90bc3062e408705825cee461dc63527b483f6
siniseong <shinhuiseong07@gmail.com>
2024-10-18 09:22:46 +0900

    ✨ Config 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';  
 import { AuthModule } from './auth/auth.module';
 import { InsertModule } from './insert/insert.module';
 import { GetinsertModule } from './getinsert/getinsert.module';
@@ -12,12 +12,10 @@ import * as mongoose from 'mongoose';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
+    ConfigModule.forRoot({   
+      isGlobal: true,  
     }),
-    MongooseModule.forRoot(process.env.MONGODB_URI, {
-   
-    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI),  
     AuthModule,
     InsertModule,
     GetinsertModule,
@@ -31,7 +29,7 @@ export class AppModule implements NestModule {
   private readonly isDev: boolean = process.env.NODE_ENV === 'dev';
 
   configure() {
-    console.log('MONGODB_URI:', process.env.MONGODB_URI);
+    console.log('MONGODB_URI:', process.env.MONGODB_URI); 
     mongoose.set('debug', this.isDev);
   }
 }
