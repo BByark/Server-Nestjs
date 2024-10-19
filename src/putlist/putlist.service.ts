d87e0dd226444f7472415217cc1549f8e31d7fac
huise0ng <shinhuiseong07@gmail.com>
2024-10-19 23:42:06 +0900

    ♻️ Note 값 한번에 받아오도록 수정

--- src/putlist/putlist.service.ts
+++ src/putlist/putlist.service.ts
@@ -1,8 +1,8 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { Note } from '../notes/schemas/note.schema';
-import { UpdateNoteDto } from './dto/update-note.dto';
+import { InjectModel } from '@nestjs/mongoose'; 
+import { Model } from 'mongoose'; 
+import { Note } from '../notes/schemas/note.schema'; 
+import { UpdateNoteDto } from './dto/update-note.dto'; 
 
 @Injectable()
 export class PutlistService {
@@ -14,15 +14,21 @@ export class PutlistService {
       throw new NotFoundException(`Note with id ${id} not found`);
     }
 
-    note.class = updateNoteDto.class;
-    note.facilitator = updateNoteDto.facilitator;
-    note.recorder = updateNoteDto.recorder;
-    note.class_size = updateNoteDto.class_size;
-    note.absent_students = updateNoteDto.absent_students;
-    note.meeting_content = updateNoteDto.meeting_content;
-    note.meeting_result = updateNoteDto.meeting_result;
-    note.additional_requests = updateNoteDto.additional_requests;
+    note.set(updateNoteDto);
 
     return note.save();
   }
 }
+
+// 요청 양식
+// {
+//   "class": "24",
+//   "facilitator": "string",
+//   "recorder": "string",
+//   "class_size": 0,
+//   "absent_students": "string",
+//   "meeting_content": "string",
+//   "meeting_result": "string",
+//   "additional_requests": "hello"
+// }
+
